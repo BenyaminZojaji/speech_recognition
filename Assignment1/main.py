cc8f2a10d0cec8c640d2d95575bcf7e5f59b73d6
Benyamin Zojaji <77120507+BenyaminZojaji@users.noreply.github.com>
2021-12-24 01:55:25 +0330

    Assignment1

---
+++ Assignment1/main.py
@@ -0,0 +1,18 @@
+import speech_recognition
+import pyttsx3
+
+recognizer = speech_recognition.Recognizer()
+
+while True:
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+            audio = recognizer.listen(mic)
+
+            text = recognizer.recognize_google(audio)
+            text = text.lower()
+            print(f'Recognized: {text}')
+    except:
+        print('error')
+        recognizer = speech_recognition.Recognizer()
+        continue
